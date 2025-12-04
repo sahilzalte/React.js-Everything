d21999b48025a1c782d4ccc0dcb0f58d4b5e6140
Sahil Zalte <zaltesahil94@gmail.com>
2025-12-04 21:40:57 +0530

    08-functions Completed

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+
+
+
+  return (
+    <div>
+      <input type="text" name="" id=""
+      placeholder="Enter Name" onChange={(e)=>{
+        console.dir(e.target.value);
+      }} />
+    </div >
+  )
+}
+
+export default App
