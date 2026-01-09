0bb421a2980341295179cd36ece8d967e1a00a03
Sahil Zalte <zaltesahil94@gmail.com>
2026-01-09 22:28:10 +0530

    Add axios and Tailwind CSS dependencies
    Added axios, tailwindcss, and @tailwindcss/vite to package.json for HTTP requests and styling support. Updated package-lock.json accordingly.

--- 17-galary-project/src/App.jsx
+++ 17-galary-project/src/App.jsx
@@ -1,6 +1,17 @@
+import React from 'react'
+import axios from 'axios'
+
 const App = () => {
+
+  const getData = async() => {
+    let response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    console.log(response)
+  }
+
   return (
-    <div>App</div>
+    <div className="bg-black h-screen p-4 text-white">
+      <button onClick={getData} className="bg-green-600 text-white px-5 py-2 mb-2 rounded active:scale-95 cursor-pointer">Get Data</button>
+    </div>
   )
 }
 
