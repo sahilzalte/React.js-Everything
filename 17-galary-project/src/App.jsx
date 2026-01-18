55f82bf311e489354297bd1843e391b17077fffe
Sahil Zalte <zaltesahil94@gmail.com>
2026-01-18 20:33:02 +0530

    Update gallery app to fetch and display images
    Changed the document title to 'Galary Project'. Updated App.jsx to fetch a list of images from the Picsum API, display them in a responsive grid, and show author names. Added basic gallery layout and improved data handling.

--- 17-galary-project/src/App.jsx
+++ 17-galary-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
@@ -6,12 +6,37 @@ const App = () => {
 
 
   const getData = async () => {
-    let response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    let response = await axios.get('https://picsum.photos/v2/list?page=3&limit=15')
     setUserData(response.data);
   }
+
+  console.log(userData);
+
+  let printUserData = 'No user Available';
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+
+      return <div key={idx} >
+        <a href={elem.url} target='_blank'>
+          <div className='h-40 w-44 overflow-hidden bg-white rounded-xl' key={idx}>
+            <img className='h-full w-full object-cover' src={elem.download_url} alt="" />
+          </div>
+          <h2 className='font-bold text-lg'>{elem.author}</h2>
+        </a>
+      </div>
+    })
+  }
+
   return (
-    <div className="bg-black h-screen p-4 text-white">
+    <div className="bg-black overflow-auto h-screen p-4 text-white">
       <button onClick={getData} className="bg-green-600 text-white px-5 py-2 mb-2 rounded active:scale-95 cursor-pointer">Get Data</button>
+
+      <div className="flex flex-wrap gap-4">
+
+        {printUserData}
+
+      </div>
     </div>
   )
 }
